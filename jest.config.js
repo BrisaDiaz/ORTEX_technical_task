d3d16298003b1e6f3efb844cf24f8b76f534770e
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-02-23 12:45:28 -0300

    removing ts-jest module

--- jest.config.js
+++ jest.config.js
@@ -23,9 +23,6 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   verbose: true,
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
